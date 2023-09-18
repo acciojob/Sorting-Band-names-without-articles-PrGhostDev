bcbae4c3fe379df6ca13b6a69d7dcaf6f59aae27
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-18 22:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Pink Floyd', 'The Beatles', 'Aerosmith'];
+
+function sortBandNames(names) {
+
+  const articleRegex = /^(a|an|the)\s+/i;
+
+  const sortedNames = names.map((name) => name.replace(articleRegex, ''));
+
+  sortedNames.sort((a, b) => a.localeCompare(b));
+
+  return sortedNames;
+}
+
+const ulElement = document.getElementById('bands');
+
+const sortedBandNames = sortBandNames(bandNames);
+
+sortedBandNames.forEach((name) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
 
